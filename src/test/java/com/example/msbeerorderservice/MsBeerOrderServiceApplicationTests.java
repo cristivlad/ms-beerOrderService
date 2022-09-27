372527e4df0c3ffe5b8df1b0c0422758c3a0ba42
cristiii <cristian.vlad10@gmail.com>
2022-09-27 20:59:02 +0300

    init

---
+++ src/test/java/com/example/msbeerorderservice/MsBeerOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msbeerorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBeerOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
