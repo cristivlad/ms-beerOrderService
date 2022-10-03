b6093f15f390545589c38d4bfebda58c20966424
cristiii <cristian.vlad10@gmail.com>
2022-10-03 22:25:19 +0300

    updated to use postgresql container

--- src/main/java/com/example/msbeerorderservice/web/mappers/BeerOrderMapper.java
+++ src/main/java/com/example/msbeerorderservice/web/mappers/BeerOrderMapper.java
@@ -8,7 +8,7 @@ import org.mapstruct.Mapping;
 @Mapper(uses = {DateMapper.class, BeerOrderLineMapper.class})
 public interface BeerOrderMapper {
 
-    @Mapping(target = "customerId", source = "customer.id")
+//    @Mapping(target = "customerId", source = "customer.id")
     BeerOrderDto beerOrderToDto(BeerOrder beerOrder);
 
     BeerOrder dtoToBeerOrder(BeerOrderDto dto);
